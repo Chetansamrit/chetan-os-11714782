74fd85a03f0492d91e7592135a7b6ff9d01c2590
Chetansamrit <bhojrajsamrit@gmail.com>
2019-03-29 07:24:28 +0530

    Create os1.c

---
+++ os1.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+int main() 
+{ 
+int i, j, x, b , z, limit, total = 0, count = 0, tq, wt=0, tat=0, pos, p[10], prio[10], at[10], bt[10], temp[10];
+
+float awt, atat;
+
+printf("\n ----====::Enter Total Number of Processes::====----");
+
+scanf("%d", &limit);
+
+x=limit;
+
+
+}
