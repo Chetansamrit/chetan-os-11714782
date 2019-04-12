79d8623995b415bf7ffe12128c33a092f1cb2031
Chetansamrit <bhojrajsamrit@gmail.com>
2019-04-12 21:22:09 +0530

    osupdate mark4

--- os1.c
+++ os1.c
@@ -1,17 +1,20 @@
 #include<stdio.h> 
+ //final
 int main() 
 { 
-int i, j, x, b , z, limit, total = 0, count = 0, tq, wt=0, tat=0, pos, p[10], prio[10], at[10], bt[10], temp[10];
-
-float awt, atat;
-
-printf("\n ----====::Enter Total Number of Processes::====----");
-
-scanf("%d", &limit);
-
-x=limit;
-
-for(i = 0; i < limit; i++) 
+      int i, limit, total = 0, x, count = 0, tq,j; 
+      
+	  int wt = 0, tat = 0,pos,z,p[10],prio[10], at[10], bt[10], temp[10],b; 
+      
+	  float average_wt, average_tat;
+      
+	  printf("\nEnter Total Number of Processes:"); 
+      
+	  scanf("%d", &limit); 
+      
+	  x = limit; 
+--------------------
+      for(i = 0; i < limit; i++) 
       {
 	    p[i]=i+1;
 	   
@@ -23,7 +26,11 @@ for(i = 0; i < limit; i++)
             scanf("%d", &bt[i]); 
             temp[i] = bt[i];
       }
- for(total = 0, i = 0; x != 0;) 
+	   
+      printf("\nEnter the Time Quantum:"); 
+      scanf("%d", &tq); 
+      printf("\nProcess ID\t\tBurst Time\t Turnaround Time\t Waiting Time\t Priority\n");
+      for(total = 0, i = 0; x != 0;) 
       { 
 
 		    for(z=0;z<limit;z++)
@@ -42,12 +49,12 @@ for(i = 0; i < limit; i++)
 	
 		prio[pos]=temp1;
 		 
-			temp1=b_time[z];
-			b_time[z]=b_time[pos];
-			b_time[pos]=temp1;
-		 			temp1=a_time[z];
-				a_time[z]=a_time[pos];
-			a_time[pos]=temp1;
+			temp1=bt[z];
+			bt[z]=bt[pos];
+			bt[pos]=temp1;
+		 			temp1=at[z];
+				at[z]=at[pos];
+			at[pos]=temp1;
 
 			temp1=p[z];
 				p[z]=p[pos];
@@ -57,7 +64,53 @@ for(i = 0; i < limit; i++)
 				temp[z]=temp[pos];
 			temp[pos]=temp1;
 		    }
-return 0; 
+		{
+		}
+            
+			if(temp[i] <= tq && temp[i] > 0) 
+            { 
+                  total = total + temp[i]; 
+                  temp[i] = 0; 
+                  count = 1; 
+            } 
+            
+			else if(temp[i] > 0) 
+            { 
+                  temp[i] = temp[i] - tq; 
+                  total = total + tq; 
+            } 
 
+	for(b=0;b<limit;b++)
+		{
+			if(b==i)
+			prio[b]+=1;
+			else
+			prio[b]+=2;
+		}
 
+            if(temp[i] == 0 && count == 1) 
+            { 
+                  x--; 
+                  printf("\nProcess[%d]\t\t%d\t\t %d\t\t %d\t\t%d", p[i], bt[i], total - at[i], total - at[i] - bt[i],prio[i]);
+                  wt = wt + total - at[i] - bt[i]; 
+                  tat = tat + total - at[i]; 
+                  count = 0; 
+            } 
+            if(i == limit - 1) 
+            {
+                  i = 0; 
+            
+			}
+            else if(at[i + 1] <= total) 
+            {
+                  i++;
+            
+			}
+            else 
+            {
+                  i = 0;
+            
+			}		
+      } 
+      return 0; 
 }
